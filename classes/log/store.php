df7169b690d1e30c7bdd536bb4636e15594833ef
Thomas Dondorf <dondorf@win.lfi.rwth-aachen.de>
2020-03-11 11:14:49 +0100

    Changed User-ID to be always set to 0

--- classes/log/store.php
+++ classes/log/store.php
@@ -96,7 +96,7 @@ class store implements \tool_log\log\writer {
             $record->courseid = $event['courseid'];
             $record->objectid = $event['objectid'];
             $record->contextid = $event['contextid'];
-            $record->userid = $event['userid'];
+            $record->userid = 0; // $event['userid']; // Anonymize log entries everything.
             $record->os = $event['os'];
             $record->browser = $event['browser'];
             $records[] = $record;
